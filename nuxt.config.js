041c052bdc354a0e2f7cd13fbb62b13d0c75f73c
MilttO0nMss <grountish@gmail.com>
2021-03-22 22:51:51 +0100

    prev

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,7 @@ export default {
 
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ['~/plugins/preview.client.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -49,6 +48,7 @@ export default {
 sanity: {
   projectId: 'ygcad2ic',
   dataset:'production',
-  minimal: true
+  minimal: true,
+  useCdn: false,
   },
 }
