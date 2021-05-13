fd3183fba649006cb76c48873798a49d4db76e85
Sofia Santa Maria <yolaresofia@gmail.com>
2021-05-13 21:57:45 +0200

    settimeoutarregladomentiraaverahora

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,14 +1,15 @@
 <template>
   <nav
-    class="flex items-center justify-between absolute flex-wrap bg-white py-1 px-10 w-full z-10 top-0"
+    class="flex items-center justify-between absolute flex-wrap  bg-white py-1 px-10 w-full z-10 top-0"
   >
     <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <img src="../static/logo.png" alt="" class="h-8 w-8 mx-3" />
       <NuxtLink
-        class="text-blue-800 no-underline logo hover:text-green-600 hover:no-underline"
+        class="text-black no-underline logo hover:text-PrimaryGreen hover:no-underline"
         to="/"
       >
-        <span class="text-2xl font-bold pl-2">Disco Digital</span>
+        <span class="text-2xl font-bold pl-2"
+          ><i class="em em-grinning"></i> Disco Digital</span
+        >
       </NuxtLink>
     </div>
 
@@ -16,7 +17,7 @@
       <button
         id="nav-toggle"
         @click="toggleNav"
-        class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+        class="flex items-center px-3 py-2 border rounded text-gray-500 focus:outline-none border-gray-600 hover:text-black hover:border-black"
       >
         <svg
           class="fill-current h-3 w-3"
@@ -31,61 +32,66 @@
 
     <div
       :class="[
-        toggle ? 'h-40' : 'h-0',
-        'w-full flex-grow lg:flex lg:items-center lg:w-auto lg:pt-0',
-      ]"
-      id="nav-content"
-    >
+        toggle ? 'h-40' : 'h-0','w-full flex-grow lg:flex lg:items-center lg:w-auto lg:pt-0',]" id="nav-content">
       <ul
-        :class="[
-          linkse ? 'opacity-100' : 'opacity-0 sm:hidden',
-          'pt-4 lg:pt-0 lg:opacity-100 list-reset lg:flex justify-end flex-1 items-center uppercase text-xs text-PrimaryGreen',
-        ]"
+        :class="[linkse ? ' opacity-100 ': ' opacity-0 sm:hidden  ','pt-4 lg:pt-0 lg:opacity-100 list-reset lg:flex justify-end flex-1 items-center uppercase text-xs text-PrimaryGreen' ]"
       >
+      
+     
         <li class="mr-3">
           <NuxtLink
-            to="/omoss"
-            class="inline-block no-underline transition-all duration-200 hover:text-blue-800 py-2 px-4"
-            >Om Oss</NuxtLink
+            class="inline-block no-underline transition-all duration-200 hover:text-green-900 py-2 px-4"
+            to="/search"
+            >search</NuxtLink
           >
+          <span class=" hidden lg:inline-block">|</span>
         </li>
+
         <div v-for="lin in xtraLinks" :key="lin.name">
           <li class="mr-3">
             <NuxtLink
-              class="inline-block no-underline transition-all duration-200 hover:text-blue-800 py-2 px-4"
+              class="inline-block no-underline transition-all duration-200 hover:text-green-900 py-2 px-4"
               :to="'/pages/' + lin.name"
               >{{ lin.name }}</NuxtLink
             >
+            <span class=" hidden lg:inline-block">|</span>
           </li>
         </div>
+          <li class="mr-3">
+          <NuxtLink
+            to="/about"
+            class="inline-block no-underline transition-all duration-200 hover:text-green-900 py-2 px-4"
+            >Om oss</NuxtLink
+          >
+        </li>
       </ul>
+     
     </div>
   </nav>
 </template>
 
 <script>
 import { groq } from "@nuxtjs/sanity";
-
 const query = groq`*[_type=="xtraPage"]`;
 
 export default {
+  components: { SanityContent },
   data: () => ({
     toggle: false,
     xtraLinks: "",
-    linkse: false,
+    linkse:false
   }),
   methods: {
     toggleNav() {
       this.toggle = !this.toggle;
-      this.linkse
-        ? this.linkse = !this.linkse
-        : setTimeout(() => {
-            this.linkse = !this.linkse;
-          }, 3000);
+      this.linkse ?  this.linkse = !this.linkse
+     : setTimeout(() => {
+        this.linkse = !this.linkse
+      }, 300);
     },
   },
   async fetch() {
-    this.xtraLinks = await this.$sanity.fetch(query);
+    const xtraLinks = await this.$sanity.fetch(query);
     this.xtraLinks = xtraLinks;
   },
 };
@@ -95,21 +101,17 @@ export default {
 nav * {
   font-family: "Space Grotesk", sans-serif;
 }
-
-#nav-content {
-  transition: all 0.31s;
+#nav-content{
+  transition: all .31s;
 }
-
 #nav-content ul {
-  transition: all 0.11s;
+  transition: all .11s;
 }
-
 a.nuxt-link-exact-active {
-  color: #392f70;
+  color: #305a39;
   border-bottom: 1px solid gray;
 }
-
-.logo {
+.logo{
   border-bottom: none !important;
 }
 </style>
