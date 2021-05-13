65af00c6718d754c8dbe148e608b728f1ccf5c8e
Sofia Santa Maria <yolaresofia@gmail.com>
2021-05-13 20:48:12 +0200

    navbar transitions added

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -3,12 +3,13 @@
     class="flex items-center justify-between absolute flex-wrap bg-white py-1 px-10 w-full z-10 top-0"
   >
     <div class="flex items-center flex-shrink-0 text-white mr-6">
+      <img src="../static/logo.png" alt="" class="h-8 w-8 mx-3">
       <NuxtLink
-        class="text-blue-800 no-underline hover:text-green-600 hover:no-underline"
+        class="text-blue-800 no-underline logo hover:text-green-600 hover:no-underline"
         to="/"
       >
         <span class="text-2xl font-bold pl-2"
-          ><i class="em em-grinning"></i> Disco Digital</span
+          >Disco Digital</span
         >
       </NuxtLink>
     </div>
@@ -32,13 +33,13 @@
 
     <div
       :class="[
-        toggle ? '' : 'hidden',
-        'w-full flex-grow lg:flex lg:items-center lg:w-auto pt-6 lg:pt-0',
+        toggle ? 'h-40' : 'h-0',
+        'w-full flex-grow lg:flex lg:items-center lg:w-auto lg:pt-0',
       ]"
       id="nav-content"
     >
       <ul
-        class="list-reset lg:flex justify-end flex-1 items-center uppercase text-xs text-black"
+        :class="[linkse ? 'opacity-100' : 'opacity-0 sm:hidden', 'pt-4 lg:pt-0 lg:opacity-100 list-reset lg:flex justify-end flex-1 items-center uppercase text-xs text-PrimaryGreen']"
       >
         <li class="mr-3">
           <NuxtLink
@@ -64,34 +65,27 @@
 
 <script>
 import { groq } from "@nuxtjs/sanity";
-import { SanityContent } from "@nuxtjs/sanity/dist/components/sanity-content";
-import Categories from './Categories.vue';
+
 const query = groq`*[_type=="xtraPage"]`;
 
 export default {
-  components: { SanityContent, Categories },
-
-  async asyncData({ $sanity }) {
-    const page = await $sanity.fetch(query);
-  },
   data: () => ({
     toggle: false,
     xtraLinks:'',
-    navLinks: [
-      { name: "art", link: "/pages/art" },
-      { name: "soul", link: "/pages/soul" },
-    ],
+    linkse: false,
   }),
   methods: {
     toggleNav() {
       this.toggle = !this.toggle;
+      this.linkse ? this.linkse = !this.linkse 
+      : setTimeout(()=> {
+        this.linkse = !this.linkse
+      }, 300)
     },
-     async fetchSomething() {
-    const data = await this.$axios.$get('https://qp66vak1.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D%22xtraPage%22%5D')
-    this.xtraLinks = data.result
+     async fetch() {
+    this.xtraLinks = await this.$sanity.fetch(query)
+    this.xtraLinks = xtraLinks
   }
-  },mounted(){
-    this.fetchSomething()
   }
 };
 </script>
@@ -101,8 +95,20 @@ nav * {
   font-family: "Space Grotesk", sans-serif;
 }
 
+#nav-content {
+  transition: all .31s;
+}
+
+#nav-content ul {
+  transition: all .11s;
+}
+
 a.nuxt-link-exact-active {
   color: #392f70;
   border-bottom: 1px solid gray;
+}
+
+.logo {
+  border-bottom: none !important;
 }
 </style>
