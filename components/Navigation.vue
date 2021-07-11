a7d82c423c039bf8e3c0b0b3eec2631ce514b49e
Sofia Santa Maria <yolaresofia@gmail.com>
2021-07-11 19:59:28 +0200

    search component fixed in mobile

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
   <nav
-    class="absolute top-0 z-10 flex flex-wrap shadow items-center justify-between w-full px-10 py-1 bg-white"
+    :class="[toggle?'z-20':' z-0','absolute top-0  flex flex-wrap items-center justify-between w-full px-2 lg:px-10 py-1 bg-white']"
   >
     <div class="flex items-center flex-shrink-0 mr-6 text-white">
       <NuxtLink
-        class="text-PrimaryGreen no-underline transition-all duration-300 logo hover:text-green-400 hover:no-underline"
+        class="text-black no-underline logo hover:text-PrimaryGreen hover:no-underline"
         to="/"
       >
         <span class="pl-2 text-2xl font-bold"
@@ -13,37 +13,44 @@
       </NuxtLink>
     </div>
 
-    <div :class="[toggle ? 'block lg:hidden m-3 bg-white text-PrimaryGreen rounded-md border-bg-PrimaryGreen border':'block lg:hidden m-3  bg-PrimaryGreen rounded-md', ]">
-      <div
+    <div class="block lg:hidden">
+      <button
         id="nav-toggle"
         @click="toggleNav"
-        class="flex items-center px-3  py-2 rounded focus:outline-none hover:text-PrimaryGreen "
+        class="flex items-center px-3 py-2 text-black border border-gray-600 rounded focus:outline-none hover:text-black hover:border-black"
       >
         <svg
-          :class="[toggle ? 'text-PrimaryGreen':'fill-current', 'w-3 h-3  ']"
+          class="w-3 h-3 fill-current"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
         >
           <title>Menu</title>
           <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
         </svg>
-      </div>
+      </button>
     </div>
 
     <div
       :class="[
-        toggle ? ' h-36' : 'h-0',
+        toggle ? 'h-40' : 'h-0',
         'w-full flex-grow lg:flex lg:items-center lg:w-auto lg:pt-0',
       ]"
       id="nav-content"
     >
       <ul
         :class="[
-          linkse ? ' opacity-100 ' : ' opacity-0 sm:hidden  ',
+          linkse ? ' opacity-100 ' : ' opacity-0 hidden  ',
           'pt-4 lg:pt-0 lg:opacity-100 list-reset lg:flex justify-end flex-1 items-center uppercase text-xs text-PrimaryGreen',
         ]"
       >
-      
+        <li class="mr-3">
+          <NuxtLink
+            class="inline-block px-4 py-2 no-underline transition-all duration-200 hover:text-green-900"
+            to="/search"
+            >search</NuxtLink
+          >
+          <span class="hidden lg:inline-block">|</span>
+        </li>
 
         <div v-for="lin in xtraLinks" :key="lin.name">
           <li class="mr-3">
@@ -57,7 +64,7 @@
         </div>
         <li class="mr-3">
           <NuxtLink
-            to="/omoss"
+            to="/about"
             class="inline-block px-4 py-2 no-underline transition-all duration-200 hover:text-green-900"
             >Om oss</NuxtLink
           >
@@ -70,7 +77,6 @@
 <script>
 import { groq } from "@nuxtjs/sanity";
 const query = groq`*[_type=="xtraPage"]`;
-
 export default {
   data: () => ({
     toggle: false,
@@ -91,7 +97,10 @@ export default {
     const xtraLinks = await this.$sanity.fetch(query);
     this.xtraLinks = xtraLinks;
   },
-  
+  mounted(){
+    let unorderList = document.getElementsByTagName('ul')[0]
+    unorderList.addEventListener('click',()=>this.toggleNav())
+  }
 };
 </script>
 
